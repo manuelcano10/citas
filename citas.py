30f10e6fc15926b0c3d2fd7fa9a4e2de551ca132
manuelcano10 <manuelmonsalvecano2110@gmail.com>
2020-04-25 22:26:54 -0500

    Citas app

--- citas.py
+++ citas.py
@@ -1,16 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
 
+
+dbquotes = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="citas"
+)
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('home.html')
+def home():
+    sql = "SELECT * FROM citasapp"
+    cur = dbquotes.cursor()
+    cur.execute(sql)
+    result = cur.fetchall()
+    return render_template('home.html', quotes = result)
 
 @app.route('/citas')
 def citas():
     return render_template('assigned-appointments.html')
+
+
+@app.route('/addquote', methods={'POST'})
+def addquote():
+    if request.method == 'POST':
+        name = request.form['name']
+        surname = request.form['surname']
+        document = request.form['document']
+        birth_date = request.form['birth_date']
+        city = request.form['city']
+        neighborhood = request.form['neighborhood']
+        mobile = request.form['mobile']
+        cur = dbquotes.cursor()
+        sql = f"INSERT INTO citasapp (nombre,apellido,documento,fecha_nacimiento,ciudad,barrio,celular) VALUES ('{name}','{surname}','{document}','{birth_date}','{city}','{neighborhood}','{mobile}')"
+        cur.execute(sql)
+        dbquotes.commit()
+        return redirect(url_for('home'))
+    return "Error"
+
 
 if __name__== "__main__":
     app.run(debug=True)
