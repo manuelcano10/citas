269a1e2d8cebd8c208e6caf783a6b987a45222b4
manuelcano10 <manuelmonsalvecano2110@gmail.com>
2020-05-03 19:50:08 -0500

    Citas App

--- citas.py
+++ citas.py
@@ -1,36 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
-import mysql.connector
-
-dbquotes = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  passwd="",
-  database="citas"
-)
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'citas'
+mysql = MySQL(app)
 
+#configuraciones
+app.secret_key= 'mysecretkey'
 
 @app.route('/')
 def index():
     sql = "SELECT * FROM citasapp"
-    cur = dbquotes.cursor()
+    cur = mysql.connection.cursor()
     cur.execute(sql)
     result = cur.fetchall()
     return render_template('home.html', quotes = result)
-
-@app.route('/eliminar')
-def delete():
-    cur = dbquotes.cursor()
-    sql = "DELETE FROM citasapp"
-    cur.execute(sql)
-    dbquotes.commit()
 
 
 @app.route('/citas')
 def citas():
     return render_template('assigned-appointments.html')
 
+
+@app.route('/delete/<string:nombre>')
+def delete(nombre):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM citasapp WHERE nombre = {0}',format(nombre))
+    mysql.connection.commit()
+    flash('Cita removida con exito!')
+    return redirect(url_for('index'))
 
 @app.route('/addquote', methods={'POST'})
 def addquote():
@@ -42,10 +43,11 @@ def addquote():
         city = request.form['city']
         neighborhood = request.form['neighborhood']
         mobile = request.form['mobile']
-        cur = dbquotes.cursor()
+        cur = mysql.connection.cursor()
         sql = f"INSERT INTO citasapp (nombre,apellido,documento,fecha_nacimiento,ciudad,barrio,celular) VALUES ('{name}','{surname}','{document}','{birth_date}','{city}','{neighborhood}','{mobile}')"
         cur.execute(sql)
-        dbquotes.commit()
+        mysql.connection.commit()
+        flash('Cita agendada con Exito!')
         return redirect(url_for('index'))
     return "Error"
 
